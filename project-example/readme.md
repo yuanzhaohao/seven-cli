3cc0f642e49923f94c3d606f38fed0241885b781
yuanzhaohao <yuanzhaohao@163.com>
2017-11-08 21:28:28 +0800

    feat: dev debug

--- project-example/readme.md
+++
@@ -1,2 +0,0 @@
-## project-example
-> A new project
--- project-example/src/page/page1.js
+++
@@ -1,9 +0,0 @@
-import App from '../components/page1/index';
-
-window.addEventListener('DOMContentLoaded', function() {
-  new Vue({
-    el: '#app',
-    template: '<App />',
-    components: { App }
-  });
-});
